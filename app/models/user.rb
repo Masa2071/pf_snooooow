b5dee71925bf0b11ee096f31666eb659f202b06d
Masa2071 <zenchin207@gmail.com>
2020-12-29 11:34:41 +0000

    [add]チャット機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
 
+  has_many :user_rooms
+  has_many :chats
+
   def follow(user_id) #フォローする
     relationships.create(followed_id: user_id)
   end
